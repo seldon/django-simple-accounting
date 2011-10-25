0a2235d808a324c899dad641b476d600bebf0649
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-10-25 21:40:54 +0200

    Added license and copyright notices.

--- accounting/exceptions.py
+++ accounting/exceptions.py
@@ -1,1 +1,17 @@
+# Copyright (C) 2011 REES Marche <http://www.reesmarche.org>
+#
+# This file is part of ``django-simple-accounting``.
+
+# ``django-simple-accounting`` is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# ``django-simple-accounting`` is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with ``django-simple-accounting``. If not, see <http://www.gnu.org/licenses/>.
+
 # definitions of custom exceptions go here

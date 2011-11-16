3539298014f6262961421cc508538818e8dbe75f
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-11-16 16:11:44 +0100

    Bug fix.
    If a model class declares more than one ``ForeignKey`` to the
    same model, ``related_name``s must be given.

--- accounting/models.py
+++ accounting/models.py
@@ -669,8 +669,8 @@ class Split(models.Model):
     a single accounting system).    
     """
     
-    entry_point = models.ForeignKey(Account, null=True, blank=True)
-    exit_point = models.ForeignKey(Account, null=True, blank=True)
+    entry_point = models.ForeignKey(Account, null=True, blank=True, related_name='entry_points_set')
+    exit_point = models.ForeignKey(Account, null=True, blank=True, related_name='exit_points_set')
     target = models.ForeignKey(CashFlow)
     
     # model-level custom validation goes here

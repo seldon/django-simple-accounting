153c8caf5275b770978637f964ca4f666f6ce975
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-11-30 17:42:31 +0100

    Added another bunch of (stubbed) test-cases.

--- simple_accounting/tests/tests.py
+++ simple_accounting/tests/tests.py
@@ -87,6 +87,25 @@ def setup_test_des():
     des.pacts = pacts
      
     return des
+##----------------------------------- TEST CASES ------------------------------#
+
+class SubjectDescriptorTest(TestCase):
+    """Tests related to the ``SubjectDescriptor`` descriptor"""
+   
+    def setUp(self):
+        pass
+    
+    def testGetSucceedOnInstance(self):
+        """Check that the ``subject`` attribute can be accessed from model instances"""
+        pass
+
+    def testGetFailOnClass(self):
+        """Check that the ``subject`` attribute cannot be accessed from the model class"""
+        pass
+
+    def testSetFailIfInstance(self):  
+        """Check that the ``Subject`` attribute is read-only"""
+        pass
 
 
 class EconomicSubjectTest(TestCase):
@@ -115,6 +134,7 @@ class EconomicSubjectTest(TestCase):
     def testNonSubjectifiableModels(self):
         """A model which already defines a ``subject`` attribute cannot be made 'subjective'"""
         pass
+    
     
         
 class SubjectModelTest(TestCase):
@@ -359,4 +379,269 @@ class AccountModelValidationTest(TestCase):
 
     def testValidationFailIfAccountNameContainsPathSep(self):
         """Account names can't contain ``ACCOUNT_PATH_SEPARATOR``"""
+        pass
+
+
+class CashFlowModelTest(TestCase):
+    """Tests related to the ``CashFlow`` model class"""
+   
+    def setUp(self):
+        pass
+    
+    def testIsIncoming(self):
+        """Check that incoming flows are correctly recognized"""
+        pass
+    
+    def testIsOutgoing(self):
+        """Check that outgoing flows are correctly recognized"""
+        pass
+    
+    def testGetSystem(self):
+        """Check that the property ``.system`` works as advertised"""
+        pass   
+    
+    
+class CashFlowModelValidationTest(TestCase):
+    """Check validation logic for ``CashFlow`` model class"""
+   
+    def setUp(self):
+        pass
+    
+    def testValidationFailIfNotAccountIsStock(self):
+        """Only stock-like accounts may represent cash-flows"""
+        pass
+    
+    
+class SplitModelTest(TestCase):
+    """Tests related to the ``Split`` model class"""
+   
+    def setUp(self):
+        pass
+    
+    def testIsInternal(self):
+        """Check that internal splits are correctly recognized"""
+        pass
+    
+    def testGetTargetSystem(self):
+        """Check that the property ``.target_system`` works as advertised"""
+        pass   
+    
+    def testGetAmount(self):
+        """Check that the property ``.amount`` works as advertised"""
+        pass
+    
+    def testGetAccounts(self):
+        """Check that the property ``.accounts`` works as advertised"""
+        pass
+    
+    
+class SplitModelValidationTest(TestCase):
+    """Check validation logic for ``Split`` model class"""
+   
+    def setUp(self):
+        pass
+    
+    def testValidationFailIfEntryExitPointNullStatusDiffers(self):
+        """If ``exit point`` is null, so must be ``entry_point``"""
+        pass
+    
+    def testValidationFailIfExitPointIsNotFluxLike(self):
+        """``exit_point`` must be a flux-like account"""
+        pass
+    
+    def testValidationFailIfEntryPointIsNotFluxLike(self):
+        """``entry_point`` must be a flux-like account"""
+        pass
+    
+    def testValidationFailIfTargetIsNotStockLike(self):
+        """``target`` must be a stock-like account"""
+        pass
+    
+    def testValidationFailIfEntryPointAndTargetInDifferentAccountingSystems(self):
+        """`entry_point`` must belongs to the same accounting system as ``target``"""
+        pass
+
+
+class TransactionModelTest(TestCase):
+    """Tests related to the ``Transaction`` model class"""
+   
+    def setUp(self):
+        pass
+    
+    def testGetSplits(self):
+        """Check that the property ``.splits`` works as advertised"""
+        pass
+        
+    def testIsSplit(self):
+        """Check that split transactions are correctly recognized"""
+        pass
+    
+    def testIsInternal(self):
+        """Check that internal transactions are correctly recognized"""
+        pass
+    
+    def testIsSimple(self):
+        """Check that simple transactions are correctly recognized"""
+        pass
+    
+    def testGetLedgerEntries(self):
+        """Check that the property ``.ledger_entries`` works as advertised"""
+        pass
+   
+    def testGetReferences(self):
+        """Check that the property ``.references`` works as advertised"""
+        pass
+    
+    def testSetConfirmedOK(self):
+        """Check that a transaction can be confirmed if not already so"""
+        pass
+    
+    def testSetConfirmedFailifAlreadyConfirmed(self):
+        """If a transaction had already been confirmed, raise ``InvalidAccountingOperation``"""
+        pass
+    
+    def testAddReference(self):
+        """Check that the method ``.add_reference()`` works as advertised"""
+        pass
+    
+    def testAddReferences(self):
+        """Check that the method ``.add_references()`` works as advertised"""
+        pass
+    
+
+class TransactionModelValidationTest(TestCase):
+    """Check validation logic for ``Transaction`` model class"""
+   
+    def setUp(self):
+        pass
+    
+    def testValidationFailIfConservationOfMoneyNotSatisfied(self):
+        """Check that the *law of conservation of money* is satisfied"""
+        pass
+    
+    def testValidationFailIfExitPointsAndSourceInDifferentAccountingSystems(self):
+        """Exit-points must belong to the same accounting system as the source account"""
+        pass
+    
+    def testValidationFailIfSourceAndTargetsInDifferentAccountingSystemsAndInternal(self):
+        """For internal splits, source and target accounts must belong to the same accounting system"""
+        pass
+     
+    def testValidationFailIfAnyIsPlaceholder(self):
+        """No account involved in a transaction can be a placeholder one"""
+        pass   
+    
+    
+class TransactionReferenceModelTest(TestCase):
+    """Tests related to the ``TransactionReference`` model class"""
+   
+    def setUp(self):
+        pass
+    
+    def testUniquenessConstraints(self):
+        """Check that uniqueness constraints for ``TransactionReference`` are enforced at the DB level"""
+        pass
+    
+    
+class LedgerEntryModelTest(TestCase):
+    """Tests related to the ``LedgerEntry`` model class"""
+   
+    def setUp(self):
+        pass
+    
+    def testGetDate(self):
+        """Check that the property ``.date`` works as advertised"""
+        pass
+    
+    def testGetSplit(self):
+        """Check that the property ``.split`` works as advertised"""
+        pass
+    
+    def testGetDescription(self):
+        """Check that the property ``.description`` works as advertised"""
+        pass
+        
+    def testGetIssuer(self):
+        """Check that the property ``.issuer`` works as advertised"""
+        pass
+    
+    def testNextEntryIdForLedger(self):
+        """Check that generation of ledger IDs works as expected"""
+        pass
+    
+    def testSaveOverride(self):
+        """Check that the ``.save()`` ovveride method works as expected"""
+        pass
+
+
+class InvoiceModelTest(TestCase):
+    """Tests related to the ``Invoice`` model class"""
+   
+    def setUp(self):
+        pass
+    
+    def testGetTotalAmount(self):
+        """Check that the property ``.total_amount`` works as advertised"""
+        pass
+    
+
+class AccountingProxyTest(TestCase):
+    """Tests related to the ``AccountingProxy`` class"""
+   
+    def setUp(self):
+        pass
+    
+    def testGetSubject(self):
+        """Check that the subject to which the proxy object is attached can be accessed"""
+        pass
+    
+    def testGetAccountingSystem(self):
+        """Check that the accounting system the proxy object refers to can be accessed"""
+        pass
+    
+    def testSubclassing(self):
+        """Check that the accounting proxy works even if subclassed"""
+        pass
+    
+    def testGetAccount(self):
+        """Check that the property ``.account`` works as advertised"""
+        pass
+
+    def testPayInvoiceFailIfIssuedToAnotherSubject(self):
+        """If ``invoice`` was issued to another subject, raise ``InvalidAccountingOperation``"""
+        pass
+    
+    def testPayInvoiceFailIfGivenInvalidInstance(self):
+        """If ``invoice`` isn't an ``Invoice`` model instance, raise ``ValueError``"""
+        pass
+    
+    def testSetInvoicePayedOK(self):
+        """Check that the method ``.set_invoice_paid`` works as advertised, if arguments are fine"""
+        pass
+    
+    def testSetInvoicePayedFailIfIssuedByAnotherSubject(self):
+        """If ``invoice`` was issued by another subject, raise ``InvalidAccountingOperation``"""
+        pass
+    
+    def testSetInvoicePayedFailIfGivenInvalidInstance(self):
+        """If ``invoice`` isn't an ``Invoice`` model instance, raise ``ValueError``"""
+        pass
+
+    
+class AccountingDescriptorTest(TestCase):
+    """Tests related to the ``AccountingDescriptor`` descriptor"""
+   
+    def setUp(self):
+        pass
+    
+    def testGetSucceedOnInstance(self):
+        """Check that the ``accounting`` attribute can be accessed from model instances"""
+        pass
+
+    def testGetFailOnClass(self):
+        """Check that the ``accounting`` attribute cannot be accessed from the model class"""
+        pass
+
+    def testSetFailIfInstance(self):  
+        """Check that the ``accounting`` attribute is read-only"""
         pass
